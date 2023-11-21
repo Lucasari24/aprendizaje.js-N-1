70bf9625228d88f2bda6c204256b2b1dddbdd27d
Lucas Galimberti <145685414+Lucasari24@users.noreply.github.com>
2023-11-21 01:29:55 -0300

    1 archivo HTML- 1 archivo CSS - 1 archivo JS

---
+++ index.js
@@ -0,0 +1,9 @@
+function azul() {
+	document.querySelector("#caja").style.background = "blue";
+}
+function amarillo() {
+	document.querySelector("#caja").style.background = "yellow";
+}
+function rojo() {
+	document.querySelector("#caja").style.background = "red";
+}
